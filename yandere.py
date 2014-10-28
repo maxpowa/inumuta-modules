4ebffb089a6e23616b19ca4c668e38d1e7d71fa4
Max Gurela <maxpowa1@gmail.com>
2014-10-28 02:20:52 -0400

    Add yande.re module, does pretty much the same as the gelbooru module.

---
+++ yandere.py
@@ -0,0 +1,80 @@
+"""
+yandere.py - People on IRC will love your leggings fetish!
+Copyright 2014 Max Gurela
+Adapted for use with Willie from https://github.com/infinitylabs/uguubot/blob/master/plugins/yandere.py
+
+Licensed under the Eiffel Forum License 2 (It's GPL compatible!).
+"""
+from willie import web
+from willie.module import commands, rule
+from bs4 import BeautifulSoup
+import urllib2
+import re
+import random
+
+yandere_cache = []
+
+def refresh_cache():
+    "gets a page of random yande.re posts and puts them into a dictionary "
+    url = 'https://yande.re/post?page=%s' % random.randint(1,11000)
+    soup = get_soup(url)
+
+    for result in soup.findAll('li'):
+        title = result.find('img', {'class': re.compile(r'\bpreview\b')}) #['title']
+        img = result.find('a', {'class': re.compile(r'\bdirectlink\b')}) #['href']
+        if img and title:
+            title = title['title'].replace('Rating: ', '', 1)
+            rating = title.split(' Score: ')
+            score = rating[1].split(' Tags: ')
+            tags = score[1].split(' User: ')
+            user = tags[1]
+            yandere_cache.append((result['id'].replace('p',''), rating[0], score[0], tags[0], user, img['href']))
+
+def get_yandere_tags(inp):
+    url = 'https://yande.re/post?tags=%s' % inp.replace(' ','+')
+    soup = get_soup(url)
+    imagelist = soup.find('ul', {'id': 'post-list-posts'}).findAll('li')
+    image = imagelist[random.randint(0,len(imagelist)-1)]
+    imageid = image["id"].replace('p','')
+    title = image.find('img')['title']
+    rating = title.replace('Rating: ', '', 1).split(' Score: ')
+    score = rating[1].split(' Tags: ')
+    tags = score[1].split(' User: ')
+    user = tags[1]
+    src = image.find('a', {'class': 'directlink'})["href"]
+    
+    rating = rating[0].strip()
+    if rating == 'Explicit': rating = "\x02\x034NSFW\x03\x02"
+    elif rating == 'Questionable': rating = "\x02\x037Questionable\x03\x02"
+    elif rating == 'Safe': rating = "\x02\x033Safe\x03\x02"
+
+    return (u'\x02[Yande.re]\x02 Score: \x02{}\x02 | Rating: {} | Tags: {} | https://yande.re/post/show/{}'.format(score[0], rating, tags[0].strip(), imageid))
+
+@commands('yandere', 'yande.re')
+def yandere(bot, trigger):
+    """
+    .yandere [tags] -- Yande.re -- Gets a random image from yande.re (May return NSFW images)
+    """
+
+    if trigger.group(2): 
+        bot.say(get_yandere_tags(trigger.group(2)))
+        return
+
+    id, rating, score, tags, user, image = yandere_cache.pop()
+    
+    if rating == 'Explicit': rating = "\x02\x034NSFW\x03\x02"
+    elif rating == 'Questionable': rating = "\x02\x037Questionable\x03\x02"
+    elif rating == 'Safe': rating = "\x02\x033Safe\x03\x02"
+    
+    bot.say(u'\x02[Yande.re]\x02 Score: \x02{}\x02 | Rating: {} | Tags: {} | https://yande.re/post/show/{}'.format(score, rating, tags, id))
+    if len(yandere_cache) < 3:
+        refresh_cache()
+
+def get_soup(url):
+    return BeautifulSoup(web.get(url), 'lxml')
+
+def unquote(s):
+    return urllib2.unquote(s)
+    
+#do an initial refresh of the cache
+refresh_cache()
