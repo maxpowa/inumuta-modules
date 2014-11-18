6537b39921fa0851383cdf681c407e17c8f30c76
Max Gurela <maxpowa1@gmail.com>
2014-11-18 16:40:07 -0500

    flex em

---
+++ flex.py
@@ -0,0 +1,18 @@
+"""
+flex.py - FLEX YOUR DAMN DONGERS
+Copyright 2014 Max Gurela
+
+Licensed under the Eiffel Forum License 2.
+"""
+
+from willie.module import commands
+
+@commands('flex')
+def flex(bot, trigger):
+    """
+    .flex [donger] - Flex 'em
+    """
+    if trigger.group(2):
+        bot.say(u'\u1559\u0F3C\u0E88\u0644\u035C\u0E88\u0F3D\u1557 FLEX YOUR {} \u1559\u0F3C\u0E88\u0644\u035C\u0E88\u0F3D\u1557'.format(trigger.group(2).strip().upper()))
+    else:
+         bot.say(u'\u1559\u0F3C\u0E88\u0644\u035C\u0E88\u0F3D\u1557 flex your dongers \u1559\u0F3C\u0E88\u0644\u035C\u0E88\u0F3D\u1557')
