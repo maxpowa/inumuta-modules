7270be5335b2d8769232aa31b8a4a90a980aa7e0
Max Gurela <maxpowa1@gmail.com>
2015-03-24 17:01:29 -0400

    Add kernel module

---
+++ kernel.py
@@ -0,0 +1,36 @@
+# coding=utf8
+"""
+kernel.py - Show the current linux kernels
+Copyright 2014 Max Gurela
+
+Licensed under the Eiffel Forum License 2.
+"""
+from __future__ import unicode_literals
+from willie import module,web
+from willie.formatting import color
+import json
+import re
+
+@module.commands('kernel')
+def kernel(bot, trigger):
+    """
+    .kernel [branch] - Show the latest linux kernel
+    """
+    contents = web.get('https://www.kernel.org/releases.json')
+    parsed = json.loads(contents)
+    versions = []
+
+    regex = False
+    if trigger.group(2):
+        regex = re.compile(r'^.*'+re.escape(trigger.group(2))+r'.*$', re.I)
+
+    for branch in parsed['releases']:
+        if regex:
+            if not regex.match(branch['moniker']) and not regex.match(branch['version']):
+                continue
+        versions.append(u'{} ({}|{})'.format(color(branch['version'], 3),
+            branch['moniker'], branch['released']['isodate']))
+
+    message = "Linux kernel versions: "
+    message += ", ".join(versions)
+    bot.say(message)
