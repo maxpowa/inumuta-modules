475407e0afa61e013d31722da7a15b3caa53c4c5
Max Gurela <maxpowa1@gmail.com>
2015-05-02 00:10:09 -0400

    Add auto hook creation

--- github.py
+++ github.py
@@ -29,6 +29,7 @@ import json
 import re
 import datetime
 import bottle
+import urllib
 from threading import Thread
 
 '''
@@ -383,7 +384,84 @@ def webhook():
     for row in targets:
         send_formatted_message(payload, row)
 
-    return "OK"
+    return '{"channels":' + json.dumps([chan[0] for chan in targets]) + '}'
+
+
+@bottle.get('/auth')
+def handle_auth_response():
+    code = bottle.request.query.code
+    state = bottle.request.query.state
+
+    repo = state.split(':')[0]
+    channel = state.split(':')[1]
+
+    data = {'client_id': willie_instance.config.github.client_id,
+             'client_secret': willie_instance.config.github.secret,
+             'code': code}
+    raw = web.post('https://github.com/login/oauth/access_token', data, headers={'Accept':'application/json'})
+    try:
+        res = json.loads(raw)
+        
+        if 'scope' not in res:
+            raise ValueError('You\'ve already completed authorization on this repo')
+        if 'write:repo_hook' not in res['scope']:
+            raise ValueError('You didn\'t allow read/write on repo hooks!')
+        
+        access_token = res['access_token']
+        
+        data = {
+            "name": "web", 
+            "active": "true", 
+            "events": [ "*" ], 
+            "config": {
+                "url": "http://xpw.us/webhook", 
+                "content_type": "json"
+            }
+        }
+        
+        raw = web.post('https://api.github.com/repos/{}/hooks?access_token={}'.format(repo, access_token), json.dumps(data))
+        res = json.loads(raw)
+        
+        if 'ping_url' not in res:
+            if 'errors' in res:
+                raise ValueError(', '.join([error['message'] for error in res['errors']]))
+            else:
+                raise ValueError('Webhook creation failed, try again.')
+        
+        raw, headers = web.get(res['ping_url'] + '?access_token={}'.format(access_token), return_headers=True)
+        
+        title = 'Done!'
+        header = 'Webhook setup complete!'
+        body = 'That was simple, right?! You should be seeing a completion message in {} any second now'.format(channel)
+        flair = 'There\'s no way it was that easy... things are never this easy...'
+    except Exception as e:
+        title = 'Error!'
+        header = 'Webhook setup failed!'
+        body = 'Please try using the link in {} again, something went wrong!'.format(channel)
+        flair = str(e)
+
+    page = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>{title}</title>
+    <style>
+      body {{
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+      }}
+    </style>
+  </head>
+  <body>
+    <h1>{header}</h1>
+    <p>{body}</p>
+    <small><em>{flair}</em></small>
+  </body>
+</html>
+    '''
+    
+    return page.format(title=title, header=header, body=body, flair=flair)
 
 
 @commands('gh-hook')
@@ -401,7 +479,19 @@ def configure_repo_messages(bot, trigger):
 
     channel = trigger.sender.lower()
     repo_name = trigger.group(3).lower()
+    
+    if not '/' in repo_name:
+        return bot.say('Invalid repo formatting, see ".help gh-hook" for an example')
+    
     enabled = True if not trigger.group(4) or trigger.group(4).lower() == 'enable' else False
+
+    auth_data = {
+        'client_id': bot.config.github.client_id, 
+        'scope': 'write:repo_hook',
+        'state': '{}:{}'.format(repo_name,channel)
+        }
+    auth_url = 'https://github.com/login/oauth/authorize?{}'.format(
+            urllib.urlencode(auth_data))
 
     conn = bot.db.connect()
     c = conn.cursor()
@@ -411,16 +501,14 @@ def configure_repo_messages(bot, trigger):
     if not result:
         c.execute('''INSERT INTO gh_hooks (channel, repo_name, enabled) VALUES (?, ?, ?)''', (channel, repo_name, enabled))
         bot.say("Successfully enabled listening for {repo}'s events in {chan}.".format(chan=channel, repo=repo_name))
-        bot.say('Great! Now add http://xpw.us/webhook as a webhook for your repository and ' +
-                'I\'ll send a message in here when I recieve the initial webhook ping!')
-        bot.say('You can configure the colors that I use to display webhooks with .gh-hook-color')
+        bot.say('Great! Please allow me to create my webhook by authorizing via this link: ' + shorten_url(auth_url))
+        bot.say('Once that webhook is successfully created, I\'ll post a message in here. Give me about a minute or so to set it up after you authorize. You can configure the colors that I use to display webhooks with .gh-hook-color')
     else:
         c.execute('''UPDATE gh_hooks SET enabled = ? WHERE channel = ? AND repo_name = ?''', (enabled, channel, repo_name))
         bot.say("Successfully {state} the subscription to {repo}'s events".format(state='enabled' if enabled else 'disabled', repo=repo_name))
         if enabled:
-            bot.say('Great! If you haven\'t already, add http://xpw.us/webhook as a webhook for your repository. ' +
-                    'I\'ll send a message in here when I recieve the initial webhook ping!')
-            bot.say('You can configure the colors that I use to display webhooks with .gh-hook-color')
+            bot.say('Great! Please allow me to create my webhook by authorizing via this link: ' + shorten_url(auth_url))
+            bot.say('Once that webhook is successfully created, I\'ll post a message in here. Give me about a minute or so to set it up after you authorize. You can configure the colors that I use to display webhooks with .gh-hook-color')
     conn.commit()
     conn.close()
 
@@ -761,7 +849,7 @@ def fmt_watch_message(payload=None):
 
 def shorten_url(url):
     try:
-        res, headers = web.post('http://git.io', 'url=' + url, return_headers=True)
+        res, headers = web.post('http://git.io', 'url=' + web.quote(url), return_headers=True)
         return headers['location']
     except:
         return url
