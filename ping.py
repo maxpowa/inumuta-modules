1e64323147acab74573953993fc7ed3e3b461e0e
Max Gurela <maxpowa1@gmail.com>
2016-01-09 00:54:51 -0500

    Add CTCP ping ability

--- ping.py
+++ ping.py
@@ -5,8 +5,15 @@ ping.py - Sopel Ping Module
 from __future__ import unicode_literals
 
 from subprocess import Popen, PIPE
-from sopel.module import commands
+from sopel.module import commands, rule, rate, event, unblockable
+from sopel.tools import Identifier
+import time
+import sys
 
+if sys.version_info.major < 3:
+    int = long
+
+current_milli_time = lambda: int(round(time.time() * 1000))
 
 @commands('ping')
 def ping(bot, trigger):
@@ -18,3 +25,42 @@ def ping(bot, trigger):
     p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=False)
     output, error = p.communicate()
     bot.say('[Ping] ' + error)
+
+
+@commands('cping')
+def cping(bot, trigger):
+    """
+    .cping [nick] - CTCP ping an IRC user
+    """
+    target = trigger.nick
+    if trigger.group(2) and trigger.group(3):
+        target = trigger.group(3)
+
+    target = Identifier(target)
+
+    if not target.is_nick():
+        return bot.say('[cping] You may not target anything other than a nick')
+
+    bot.msg(target, '\x01PING {0} {1:016d}\x01'.format(trigger.sender, current_milli_time()))
+
+
+@rate(5)
+@rule('(\S*) (\d{16})')
+@event('NOTICE')
+@unblockable
+def read_ping_reply(bot, trigger):
+    if 'intent' not in trigger.tags:
+        return
+
+    if trigger.tags['intent'].upper() != 'PING':
+        return
+
+    target = trigger.group(1)
+    try:
+        initialtime = int(trigger.group(2))
+    except ValueError:
+        return # Not our problem
+
+    diff = float(current_milli_time() - initialtime) / 1000
+
+    bot.msg(target, '[cping] Reply from {}: {:.3}s'.format(trigger.nick, diff))
