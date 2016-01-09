e0b195c4f1e1b788fa77ec2144d39c4748886a6a
Max Gurela <maxpowa1@gmail.com>
2016-01-08 23:58:08 -0500

    Switch wolfram|alpha library

--- wolfram.py
+++ wolfram.py
@@ -1,19 +1,33 @@
 # coding=utf8
 
 from __future__ import unicode_literals
+from sopel.config.types import StaticSection, ValidatedAttribute
 from sopel.module import commands
 from sopel import web
-import tungsten
+import wolframalpha
 import json
 
 output_ids = ['DecimalApproximation', 'Result', 'ExactResult']
+
+
+class WolframSection(StaticSection):
+    app_id = ValidatedAttribute('app_id', default=None)
+
+
+def configure(config):
+    config.define_section('wolfram', WolframSection, validate=False)
+    config.wolfram.configure_setting('app_id', 'Application ID')
+
+
+def setup(bot):
+    bot.config.define_section('wolfram', WolframSection)
 
 
 @commands('wa', 'wolfram')
 def wa_query(bot, trigger):
     if not trigger.group(2):
         return bot.say('[Wolfram] You must provide a query')
-    client = tungsten.Tungsten(bot.config.wolfram.app_id)
+    client = wolframalpha.Client(bot.config.wolfram.app_id)
 
     try:
         result = client.query(trigger.group(2))
@@ -23,7 +37,7 @@ def wa_query(bot, trigger):
     for pod in result.pods:
         if pod.id not in output_ids:
             continue
-        return bot.say('{}: {}'.format(pod.title, pod.format['plaintext'][0]))
+        return bot.say('{}: {}'.format(pod.title, pod.text))
 
     if len(result.pods) > 0:
         return bot.say('[Wolfram] No text-representable result found, see http://wolframalpha.com/input/?i={}'.format(web.quote(trigger.group(2))))
