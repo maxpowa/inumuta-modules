5b083bc2330fae4730f4d5248ae4a8c67cfa2be9
Max Gurela <maxpowa1@gmail.com>
2016-01-10 14:20:36 -0500

    Fix steam errors

--- steam.py
+++ steam.py
@@ -23,23 +23,25 @@ def get_steam_info(url):
     # we get the soup manually because the steam pages have some odd encoding troubles
     headers = { 'Cookie':'lastagecheckage=1-January-1992; birthtime=694252801' }
     page = web.get(url, headers=headers)
-    soup = BeautifulSoup(page, 'lxml', from_encoding="utf-8")
+    soup = BeautifulSoup(page, 'html.parser', from_encoding="utf-8")
 
     name = soup.find('div', {'class': 'apphub_AppName'}).text
-    desc = ": " + soup.find('div', {'class': 'game_description_snippet'}).text.strip()
+    desc = soup.find('div', {'class': 'game_description_snippet'}).text.strip()
     desc = (desc[:127] + '...') if len(desc) > 130 else desc
 
     # the page has a ton of returns and tabs
     rel_date = soup.find('span', {'class': 'date'}).text.strip()
     tags = soup.find('div', {'class': 'glance_tags'}).text.strip().replace('Free to Play', '').replace('+', '').split()
-    genre = " - Genre: " + ', '.join(tags[:4])
-    date = " - Release date: " + rel_date.replace("Release Date: ", "")
-    price = soup.find('div', {'class': 'game_purchase_price price'}).text.strip()
+    genre = "Genre: " + ', '.join(tags[:4])
+    date = "Release date: " + rel_date.replace("Release Date: ", "")
+    price = 'TBA'
+    pricediv = soup.find('div', {'class': 'game_purchase_price price'})
+    if pricediv:
+        price = pricediv.text.strip()
     if not "Free to Play" in price:
         price = "Price: " + price
-    price = " - " + price
 
-    return '[Steam] {}{}{}{}{}'.format(name, desc, genre, date, price)
+    return '[Steam] {}: {} | {} | {} | {}'.format(name, desc, genre, date, price)
 
 
 @module.rule(steam_re)
